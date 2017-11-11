9e8b47019c7d84b7016d1ddfaea3f307e5c9863d
adplt <atriadplt@gmail.com>
2017-11-11 13:41:15 +0700

    edit add-keystore.sh

--- scripts/android/add-keystore.sh
+++ scripts/android/add-keystore.sh
@@ -2,4 +2,4 @@
 
 echo "Decrypting keystore."
 
-openssl aes-256-cbc -k "$KEY_STORE_PASSWORD" -in scripts/android/keystore/game-buat-dina.keystore.enc -d -a -out android/app/game-buat-dina.keystore
+openssl aes-256-cbc -k "$KEY_STORE_PASSWORD" -in ./scripts/android/keystore/game-buat-dina.keystore.enc -d -a -out ./script/android/app/game-buat-dina.keystore
